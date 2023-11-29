bed8752edf93c09190afaf00485f87baaf910e0e
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-29 17:53:50 +0530

    Added: UC1 create simple linked list

--- DataStructures.java
+++ DataStructures.java
@@ -1,6 +1,21 @@
 
 public class DataStructures {
     public static void main(String[] args) {
-
+        Node<Integer> first = new Node<>(56);
+        Node<Integer> second = new Node<>(30);
+        first.next = second;
+        Node<Integer> third = new Node<>(70);
+        second.next = third;
+        System.out.println(first.data);
+        System.out.println(first.next.data);
+        System.out.println(second.next.data);
+    }
+}
+class Node <T>{
+    int data;
+    Node<T> next;
+    Node(int data){
+        this.data = data;
+        this.next = null;
     }
 }
